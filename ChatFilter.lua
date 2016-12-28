f344fe4a11153b0ab3bc1d3cefc05123df64795f
aramunn <aramunn@users.noreply.github.com>
2016-12-27 23:05:53 -0500

    most of non ui done?

--- ChatFilter.lua
+++ ChatFilter.lua
@@ -2,52 +2,69 @@ local ChatFilter = {}
 
 local ktDataDefault = {
   bEnabled = true,
-  nMaxMessageLength = 200,
+  nMaxMessageLength = 120,
   arrWhitelist = {
     "T3",
+    
+    "WB",
+    
+    "Scorchwing",
+    "SW",
+    "Gargantua",
+    "Garg",
+    "Mechathorn",
+    "Mecha",
+    
+    "Dreamspore",
+    "DS",
+    "Metal", "Maw",
+    "MM",
+    "King", "Honey", "Grave",
+    "KHG",
+    "Zoetic",
+    "Zoe",
+    
+    "Metal", "Maw", "Prime",
+    "MMP",
+    "King", "Plush",
+    "KP",
+    "Grendelus",
+    "Grend",
+    "Kraggar",
+    "Krag",
+    
+    "Matuk",
+    "Kundar",
+    "Frostgale",
+    
+    "Guardians", "Grove",
+    "GotG",
+    "Lightspire",
+    "LS",
+    
+    "Veteran",
+    "Adventure",
+    "Dungeon",
+    "Dungeons",
+    "Vet",
+    "Vets",
+    "Fungeon",
+    "Fungeons",
+    
+    "Genetic", "Archives",
+    "GA",
+    "Datascape",
+    "DS",
+    "Redmoon", "Terror",
+    "RMT",
   },
-  arrBlacklist = {},
-}
-
-local karrDefaultWhitelist = {
-  "T3",
-  "SW",
-  "Scorchwing",
-  "Mecha",
-  "Garg",
-  "Gargantua",
-  "DS",
-  "Dreamspore",
-  "KHG",
-  "King Honey Grave",
-  "Metal Maw",
-  "MM",
-  "Zoe",
-  "Zoetic",
-  "MMP",
-  "KP",
-  "Grend",
-  "Kraggar",
-  "GA",
-  "DS",
-  "RMT",
-  "WB",
-  "Matuk",
-  "Kundar",
-  "Frostgale",
-  "GotG",
-  "Grove",
-  "Dungeon",
-  "Veteran",
-  "Vet",
-  "Vets",
-  "Fungeon",
+  -- arrBlacklist = {},
 }
 
 local addonChatLog
 local nRealmChannelId
 local tWhitelist
-local tBlacklist
+-- local tBlacklist
 
 function ChatFilter:Setup()
   addonChatLog = Apollo.GetAddon("ChatLog")
@@ -55,6 +72,7 @@ function ChatFilter:Setup()
   self:SettingsUpdated()
   --self.xmlDoc = 
   --register xml doc loaded callback?
+  Apollo.RegisterSlashCommand("cfilter", "OnSlashCommand", self)
   Apollo.RegisterSlashCommand("chatfilter", "OnSlashCommand", self)
 end
 
@@ -63,109 +81,76 @@ function ChatFilter:FindRealmChannelId()
     local eChannelType = channelCurrent:GetType()
     if eChannelType == ChatSystemLib.ChatChannel_Realm then
       nRealmChannelId = channelCurrent:GetUniqueId()
+      return
     end
   end
 end
 
 function ChatFilter:SettingsUpdated()
-  if addonChatLog and self.tData.bEnabled then
+  if addonChatLog == nil then return end
+  if nRealmChannelId == nil then return end
+  Apollo.RemoveEventHandler("ChatMessage", self)
+  if self.tData.bEnabled then
     Apollo.RegisterEventHandler("ChatMessage", "OnChatMessage", self)
-  else
-    Apollo.RemoveEventHandler("ChatMessage", self)
   end
   self:GenerateSearchLists()
 end
 
 function ChatFilter:GenerateSearchLists()
-  --for idx, strWord in ipairs(self.tData.arrWhitelist)
-  --TODO
-  --self.tData.arrWhite/Blacklist
-end
-
--- function ChatFilter:MakeTable()
-  -- for idx, strGood in ipairs(ktWhiteList) do
-    -- local strToAdd = strGood:lower()
-    -- local tPath = tSearch
-    -- for c in strToAdd:gmatch(".") do
-      -- tPath[c] = tPath[c] or {}
-      -- tPath = tPath[c]
-    -- end
-    -- tPath.isMatch = true
+  tWhitelist = {}
+  for idx, strWord in ipairs(self.tData.arrWhitelist) do
+    tWhitelist[strWord:lower()] = true
+  end
+  -- tBlacklist = {}
+  -- for idx, strWord in ipairs(self.tData.arrBlacklist) do
+    -- tBlacklist[strWord:lower()] = true
   -- end
--- end
+end
 
 function ChatFilter:OnSlashCommand(strCmd, strParam)
+  Print("TODO")
 end
 
-local function cloneTable(tData) --deep copy
-  local tClone = {}
-  for k, v in pairs(tData) do
-    if type(v) == "table" then
-      tClone[k] = cloneTable(v)
-    else
-      tClone[k] = v
+function ChatFilter:OnChatMessage(channelCurrent, tMessage)
+  if channelCurrent:GetType() ~= ChatSystemLib.ChatChannel_Nexus then return end
+  local strMessage = tostring(tMessage.arMessageSegments[1].strText)
+  if strMessage:len() > self.tData.nMaxMessageLength then return end
+  if self:SearchMessage(strMessage) then
+    self:DisplayMessage(channelCurrent, tMessage)
+  end
+end
+
+function ChatFilter:SearchMessage(strMessage)
+  for strWord in strMessage:gmatch("%w+") do
+    if tWhitelist[strWord:lower()] then
+      return true
     end
   end
-  setmetatable(tClone, getmetatable(tData))
-  return tClone
 end
 
 function ChatFilter:DisplayMessage(channelCurrent, tMessage)
   local tQueuedMessage = {
-    tMessage = cloneTable(tMessage),
+    tMessage = self:CloneTable(tMessage),
     eChannelType = channelCurrent:GetType(),
     strChannelName = channelCurrent:GetName(),
     strChannelCommand = channelCurrent:GetCommand(),
-    idChannel = channelCurrent:GetUniqueId(),
     idChannel = nRealmChannelId,
   }
   addonChatLog:HelperGenerateChatMessage(tQueuedMessage)
   addonChatLog:HelperQueueMessage(tQueuedMessage)
 end
 
-function ChatFilter:SplitString(strIn, inSplitPattern)
-  local outResults = {}
-  local theStart = 1
-  local theSplitStart, theSplitEnd = string.find( strIn, inSplitPattern, theStart )
-  while theSplitStart do
-    table.insert( outResults, string.sub( strIn, theStart, theSplitStart-1 ) )
-    theStart = theSplitEnd + 1
-    theSplitStart, theSplitEnd = string.find( strIn, inSplitPattern, theStart )
+function ChatFilter:CloneTable(tData)
+  local tClone = {}
+  for k, v in pairs(tData) do
+    if type(v) == "table" then
+      tClone[k] = self:CloneTable(v)
+    else
+      tClone[k] = v
+    end
   end
-  table.insert( outResults, string.sub( strIn, theStart ) )
-  return outResults
-end
-
-function ChatFilter:IsInSearch(strWord)
-  local tPath = tSearch
-  for c in strWord:gmatch(".") do
-    -- Print("checking: "..c)
-    if not tPath[c] then return false end
-    tPath = tPath[c]
-    -- Print(c.." is good")
-  end
-  -- Print("result: "..tostring(tPath.isMatch))
-  if tPath.isMatch then return true end
-end
-
---ignore punctuation
-
-function ChatFilter:SearchMessage(strMessage)
-  -- for idx,strWord in ipairs(self:SplitString(strMessage, "%s+")) do
-    -- if self:IsInSearch(strWord) then 
-    -- self:GoodMessage(channelCurrent, tMessage)
-    -- return
-    -- end
-  -- end
-  -- return false
-end
-
-function ChatFilter:OnChatMessage(channelCurrent, tMessage)
-  if channelCurrent:GetType() ~= ChatSystemLib.ChatChannel_Nexus then return end
-  -- if channelCurrent:GetType() ~= ChatSystemLib.ChatChannel_Say then return end
-  local strMessage = string.lower(tostring(tMessage.arMessageSegments[1].strText))
-  if strMessage:len() > self.tData.nMaxMessageLength then return end
-  self:SearchMessage(strMessage)
+  setmetatable(tClone, getmetatable(tData))
+  return tClone
 end
 
 function ChatFilter:OnSave(eLevel)
